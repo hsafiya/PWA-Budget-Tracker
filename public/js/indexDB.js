866c51a0cc27a49edd4846c7d7c339e578fa69c6
Safiyya H <safiyya.hasanova@gmail.com>
2021-09-19 14:43:21 -0500

    offlinedata

--- public/js/indexDB.js
+++ public/js/indexDB.js
@@ -6,3 +6,12 @@ request.onupgradeneeded = (e) => {
   const db = e.target.result
   db.createObjectStore('pending', { autoIncrement: true })
 }
+//after a successful creation
+request.onsuccess = (e) => {
+    db = e.target.result
+    //   check if online, and if true, update database
+    if (navigator.onLine) {
+      offlineData()
+    }
+  }
+  
