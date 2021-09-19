78f707183160ae84a8e1bf432efa29837e6571a3
Safiyya H <safiyya.hasanova@gmail.com>
2021-09-19 14:55:14 -0500

    define global contacts

---
+++ public/service-workers.js
@@ -0,0 +1,13 @@
+//define gloabl constants
+const APP_PREFIX = 'Budget-'
+const VERSION = 'version_01'
+const CACHE_NAME = APP_PREFIX + VERSION
+
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './js/index.js',
+  './css/styles.css',
+  './icons/icon-192x192.png',
+  './manifest.json',
+]
